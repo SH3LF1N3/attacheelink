5c23ba817d98bcb5d1610ee55917ed46fec0ae3f
SH3LF1N3 <computersciencegirl1911@gmail.com>
2026-02-21 01:38:43 +0300

    Added Register button

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -106,7 +106,7 @@
               <!-- /.col -->
               <div class="col-4">
                 <div class="d-grid gap-2">
-                  <button type="submit" class="btn btn-primary">Sign In</button>
+                  <button type="submit" class="btn btn-primary">Register</button>
                 </div>
               </div>
               <!-- /.col -->
@@ -116,9 +116,6 @@
 
           <div class="social-auth-links text-center mb-3 d-grid gap-2">
             <p>- OR -</p>
-            <a href="#" class="btn btn-primary">
-              <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-            </a>
             <a href="#" class="btn btn-danger">
               <i class="bi bi-google me-2"></i> Sign in using Google+
             </a>
