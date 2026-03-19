4b12efc92e8102c00c4390bdeff2892ad662bfee
Markide1 <markndwiga@gmail.com>
2026-03-19 12:22:43 +0300

    [Added:]Home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Alink;
+use App\Http\Controllers\Home;
 use App\Http\Controllers\dash\Dashboard;
 use App\Http\Controllers\dash\Opportunity;
 use App\Http\Controllers\dash\Apps;
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', [Alink::class, 'index'])->name('login');
+Route::get('/', [Home::class, 'index'])->name('home');
+Route::get('/login', [Alink::class, 'index'])->name('login');
 Route::get('/register', [Alink::class, 'register'])->name('register');
 
 
